06a82cfdcb924424c0ac1ede2ccd38b7aef689a1
박민정 <ming1320@gmail.com>
2023-09-08 14:22:30 +0900

    detail add

--- src/components/Detail_Test.js
+++ src/components/Detail_Test.js
@@ -1,8 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { styled } from 'styled-components'
+import { TypeAnimation } from 'react-type-animation';
+
+const MainBg = styled.div`
+    background-color: #d9d9d9;
+`
+const Ball = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-image: url("./images/ball/ball_background.png");
+    background-size: cover;
+    background-position: center;
+    margin: 0 auto;
+`
+const TextBg = styled.div`
+    background-color: #fff;
+    color: #000;
+    width: 800px;
+    display: flex;
+    justify-content: space-between;
+    margin: 20px auto;
+    align-items: center;
+    padding: 20px;
+`
+const Text = styled.div`
+    font-family: 'Inter';
+    p:nth-child(1){
+        font-size: 20px;
+    }
+    p:nth-child(2){
+        font-size: 14px;
+    }
+`
+const TextRight = styled(Text)`
+    text-align: right;
+`
+const CardWrap = styled.div`
+    display: flex;
+    width: 800px;
+    margin: 0 auto;
+    justify-content: space-between;
+`
+const Card = styled.div`
+    background-color: #ddd;
+    width: 250px;
+    height: 300px;
+    &.on{
+        background-color: #fff;
+        color: blueviolet;
+        transition: 0.5s;
+    }
+    p{
+        font-size: 24px;
+    }
+`
+const Typing = styled.div`
+    width: 800px;
+    height: 300px;
+    overflow: hidden;
+    margin: 0 auto;
+    background-color: #fff;
+`
+
 
 function Detail_Test() {
+    const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div>Detail_Test</div>
+    <MainBg>
+        <Ball></Ball>
+        <TextBg>
+            <Text>
+                <p>#abc</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque libero dicta nulla nihil commodi suscipit quae dolores eius praesentium eveniet?</p>
+            </Text>
+        </TextBg>
+        <CardWrap>
+            <Card className={isHovering === true ? 'on' : ''} onMouseEnter={()=>{setIsHovering(!isHovering)}} onMouseLeave={() => {setIsHovering(false)}}>
+                {isHovering === true ? (
+                    <>
+                        <p>answer1</p>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, iusto?</span>
+                    </>) 
+                    : 
+                    <>
+                        <p>quiz1</p>
+                        <span>Lorem ipsum dolor sit amet.</span>
+                    </>
+                }
+            </Card>
+        </CardWrap>
+        <TextBg>
+            <TextRight>
+                <p>#def</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque libero dicta nulla nihil commodi suscipit quae dolores eius praesentium eveniet?</p>
+            </TextRight>
+        </TextBg>
+        <Typing>
+            <TypeAnimation
+                sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    `className={isActive === true ? 'on' : ''} onClick={()=>{setIsActive(isActive === false ? true : false)}}`,
+                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                    'onClick={()=>{setIsActive(!isActive)}}',
+                    1000
+                ]}
+                wrapper="span"
+                speed={50}
+                style={{ fontSize: '1.5em', display: 'inline-block' }}
+                repeat={Infinity}
+            />
+        </Typing>
+    </MainBg>
   )
 }
 
