177a5e4d031f6598eae683cf705e50bb208ac7ca
기범석 <kbs9406@gmail.com>
2023-09-12 14:54:04 +0900

    Login.js와 Login_test.js 만듬

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login_test from '../components/Login_test'
+
+function Login() {
+  return (
+    <>
+    <Login_test></Login_test>
+    </>
+    
+  )
+}
+
+export default Login
