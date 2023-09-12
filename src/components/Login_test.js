407e37d197dddb8a1b91e332fd4266d6535448e9
기범석 <kbs9406@gmail.com>
2023-09-12 18:11:08 +0900

    회원가입 페이지 코딩 시작

--- src/components/Login_test.js
+++ src/components/Login_test.js
@@ -1,10 +1,147 @@
 import React from 'react'
+import styled from 'styled-components'
+import Login from '../pages/Login'
+
+
+const LoginBg = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-image: url(../images/login/login_bg.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+`
+const LoginNav = styled.div`
+    width: 100%;
+    height: 300px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Logo = styled.nav`
+font-family: "Monofett", monospace;
+    font-size: 40px;
+    color: whitesmoke;
+    display: inline-block;
+    cursor: pointer;
+    margin-top: 40px;
+    margin-left: 30px;
+`
+const LoginBtn = styled.span`
+    display: inline-block;
+    color: whitesmoke;
+    margin-top: 40px;
+    margin-right: 30px;
+`
+// const FormBoxWrap = styled.div`
+// width: 100%;
+// height: 100%;
+
+// `
+const FormBox = styled.div`
+ width: 600px;
+ height: 500px;
+ padding-left: 50px;
+ position: absolute;
+bottom: 50px;
+left: 50px;
+
+
+
+
+
+
+`
+
+const Id = styled.div`
+
+p{
+    color: whitesmoke;
+    margin-top: 50px;
+    font-weight: bold;
+}
+input{
+    width: 400px;
+    margin-top: 30px;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    position: relative;
+    border-bottom: 1px solid white;
+    padding-bottom: 10px;
+}
+`
+
+const Password = styled.div`
+p{
+    color: whitesmoke;
+    margin-top: 50px;
+    font-weight: bold;
+}
+input{
+    width: 400px;
+    margin-top: 30px;
+}
+`
+const Email = styled.div`
+   
+p{
+    color: whitesmoke;
+    margin-top: 50px;
+    font-weight: bold;
+}
+input{
+    width: 400px;
+    margin-top: 30px;
+}
+`
+const Yourjob = styled.div`
+
+
+` 
+const NextButton = styled.button`
+    
+    
+    `
+const  LoginDog = styled.img`
+    
+`
+
+const loginHuman= styled.img`
+`
 
 
 
 function Login_test() {
   return (
-    <div>Login_test</div>
+    <>
+        <LoginBg>
+            
+            <LoginNav>
+                <Logo>HeLL VanILLa</Logo>
+                <LoginBtn>로그인</LoginBtn>
+            </LoginNav>
+            
+
+            <FormBox>
+                <Id><p>사용할 아이디를 입력하세요.</p><input type="text" placeholder='#' name='user_id' id='user_id'></input></Id>
+                <Password><p>비밀번호를 입력하세요.</p><input type='text' name='user_password' id='user_password'></input></Password>
+                <Email><p>이메일을 입력하세요.</p><input type placeholder='example@gmail.com' name='user_email' id='user_email'></input></Email>
+                <Yourjob><p>당신의 직업은 무엇입니까?</p></Yourjob>
+
+
+
+            </FormBox>
+        
+        </LoginBg>
+            
+        
+   
+    </>
   )
 }
 
