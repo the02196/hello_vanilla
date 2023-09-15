51a5788eb9c09ff2b8ddff957bdc776d85d6a319
이기운 <slehdejfj@gmail.com>
2023-09-15 17:40:49 +0900

    스크롤구현

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -5,7 +5,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Monofett&display=swap');
 
-
+a{text-decoration:none; color:#000;}
 *{
   font-family: 'Fira Code', monospace;
 }
--- src/pages/Quick.js
+++ src/pages/Quick.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Quick_Links_Test from '../components/Quick_Links_Test'
+import Side from '../components/Side'
+import Middle from '../components/Middle'
+import styled from 'styled-components'
+const Content = styled.div`
+  
+margin-top: 90px;
+
+`
 
 function Quick() {
   return (
-    <Quick_Links_Test/>
+    <>
+    <Content>
+    <Middle/>
+    <Side/>
+    </Content>      
+    </>
   )
 }
 
