64cd4a67dbd99df5390ab6f76648c61c8cfba683
jaewan <green23616@gmail.com>
2023-10-07 00:57:12 +0900

    댓글창 제외 완료

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -45,5 +45,238 @@ function CodeBlock({ width, height, value }) {
 
   );
 }
+export function CodeBlocka({ width, height, value }) {
+  const CodeMirrorText = `<style>
+  @keyframes boxAnimation {
+    0% {left: 0%}
+    50% {left: 50%}
+    100% {left: 100%}
+  }
+  .myBall {
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+    background-color: green;
+    position:relative;
+  }
+  .animate{
+    animation-name: boxAnimation; /* 애니메이션 이름 */
+    animation-duration: 2s;/* 애니메이션 지속 시간 설정 */
+    animation-timing-function: linear;/* 타이밍 설정 */
+    animation-iteration-count: 10;/* 반복 횟수 설정 */
+  }
+</style>`;
+
+  const options = {
+    theme: "monokai",
+    lineNumbers: true,
+    mode:"htmlmixed",
+    readOnly: "true"
+  };
+
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log("value:", value);
+    console.log(viewUpdate);
+  }, []);
+
+  return (
+  
+        <CodeMirror
+          value={CodeMirrorText}
+          height={`${height}px`}
+          width={`${width}px`}
+          onClick={onChange}
+          theme={abcdef}
+          options={options}
+        />
+
+  );
+}
+
+export function CodeBlockb({ width, height, value }) {
+  const CodeMirrorText = `<script>
+  function startAnimation() {
+     const circle = document.getElementsByClassName("myBall")[0];
+     /* myBall이라는 class명을 가진 요소를 변수 circle에 넣어주기 */
+     circle.classList.add("animate");
+  /* 함수 startAnimation을 실행하면 .animate라는 class명을 더해주기 */
+  }
+  </script>`;
+
+  const options = {
+    theme: "monokai",
+    lineNumbers: true,
+    mode:"htmlmixed",
+    readOnly: "true"
+  };
+
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log("value:", value);
+    console.log(viewUpdate);
+  }, []);
+
+  return (
+  
+        <CodeMirror
+          value={CodeMirrorText}
+          height={`${height}px`}
+          width={`${width}px`}
+          onClick={onChange}
+          theme={abcdef}
+          options={options}
+        />
+
+  );
+}
+export function CodeBlockc({ width, height, value }) {
+  const CodeMirrorText = `<button onClick="startAnimation()">Start Animation</button>`;
+
+  const options = {
+    theme: "monokai",
+    lineNumbers: true,
+    mode:"htmlmixed",
+    readOnly: "true"
+  };
+
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log("value:", value);
+    console.log(viewUpdate);
+  }, []);
+
+  return (
+  
+        <CodeMirror
+          value={CodeMirrorText}
+          height={`${height}px`}
+          width={`${width}px`}
+          onClick={onChange}
+          theme={abcdef}
+          options={options}
+        />
+
+  );
+}
+export function CodeBlockd({ width, height, value }) {
+  const CodeMirrorText = `<html>
+  <head>
+    <style>
+      @keyframes boxAnimation {
+        0% {left: 0%}
+        50% {left: 50%}
+        100% {left: 100%}
+      }
+      .myBall {
+        width: 300px;
+        height: 300px;
+        border-radius: 50%;
+        background-color: green;
+        position:relative;
+      }
+      .animate{
+        animation-name: boxAnimation;
+        animation-duration: 2s;
+        animation-timing-function: linear;
+        animation-iteration-count: 10;
+      }
+  </style>
+  </head>
+  <body>
+    <div class="myBall"></div>
+    <button onClick="startAnimation()">Start Animation</button>
+  <script>
+  function startAnimation() {
+     let circle = document.getElementsByClassName("myBall")[0];
+     circle.classList.add("animate");
+  }
+  </script>
+  </body>
+  </html>`;
+
+  const options = {
+    theme: "monokai",
+    lineNumbers: true,
+    mode:"htmlmixed",
+    readOnly: "true"
+  };
+
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log("value:", value);
+    console.log(viewUpdate);
+  }, []);
+
+  return (
+  
+        <CodeMirror
+          value={CodeMirrorText}
+          height={`${height}px`}
+          width={`${width}px`}
+          onClick={onChange}
+          theme={abcdef}
+          options={options}
+        />
+
+  );
+}
+export function CodeBlocke({ width, height, value }) {
+  const CodeMirrorText = `<html>
+  <head>
+    <style>
+      @keyframes boxAnimation {
+        0% {left: 0%}
+        50% {left: 50%}
+        100% {left: 100%}
+      }
+      .myBall {
+        width: 300px;
+        height: 300px;
+        border-radius: 50%;
+        background-color: green;
+        position:absolute;
+      }
+      .animate{
+        animation-name: boxAnimation;
+        animation-duration: 2s;
+        animation-timing-function: linear;
+        animation-iteration-count: 10;
+      }
+  </style>
+  </head>
+  <body>
+    <div class="myBall"></div>
+    <button onClick="startAnimation()">Start Animation</button>
+  </body>
+  <script>
+  function startAnimation() {
+     let circle = document.getElementsByClassName("myBall")[0];
+     circle.classList.add("animate");
+  }
+  </script>
+  </html>`;
+
+  const options = {
+    theme: "monokai",
+    lineNumbers: true,
+    mode:"htmlmixed",
+    readOnly: "true"
+  };
+
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log("value:", value);
+    console.log(viewUpdate);
+  }, []);
+
+  return (
+  
+        <CodeMirror
+          value={CodeMirrorText}
+          height={`${height}px`}
+          width={`${width}px`}
+          onClick={onChange}
+          theme={abcdef}
+          options={options}
+        />
+
+  );
+}
 
 export default CodeBlock;
--- src/components/Detail_Comments.js
+++ src/components/Detail_Comments.js
@@ -31,6 +31,7 @@ import {
 } from "firebase/firestore";
 import TextAreaEdit from "./TextAreaEdit";
 import Avatar from "../pages/Avatar";
+import { CodeBlocke } from "./CodeBlock";
 
 /*
   #### Wrappers ####
@@ -668,6 +669,7 @@ useEffect(()=>{
         <ContentWrap>
           <FetchContentTop nickname={nickname} createdate={createdate} />
           <FetchContentCenter text={text} />
+          <CodeBlocke width={"650"} height={"800"} value={"test"}></CodeBlocke>
           <FetchContentBottom />
           <TopicWrap>
             <FetchTopics />
@@ -802,9 +804,9 @@ useEffect(()=>{
             </li>
             <FetchComment
               text={
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium porro expedita deleniti itaque at quaerat modi, exercitationem vitae laboriosam."
+                "안녕하세요, 이 페이지를 보고 움직이는 공을 만들어보고 있는데 잘 동작하지 않아서 막막한 상황입니다 ㅠㅠ.아래는 제가 작성한 코드입니다.."
               }
-              nickname={"#db53632"}
+              nickname={"#ljy04"}
               createdate={"2023.09.26"}
               profile={"../images/portraits/woman_5.png"}
             />
